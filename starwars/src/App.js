ee9c027bbf7f991bdc9b2e56a2cd04ccb74375a9
furno west <furnowest57@gmail.com>
2020-06-26 18:45:32 -0700

    almost done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import Character from "./components/Character"
+import axios from "axios";
 import './App.css';
-
+import {  CardFooter, Card} from 'reactstrap';
 const App = () => {
+
+  const [character, setCharacter] = useState([]);
+
+useEffect(() => {
+  axios
+    .get("https://swapi.py4e.com/api/people/")
+    .then(response => {
+      console.log(response.data.results[0])
+      setCharacter(response.data.results)
+    })
+    .catch(err => {
+      console.log('err')
+    })
+}, []);
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,9 +28,26 @@ const App = () => {
   // sync up with, if any.
 
   return (
+    
     <div className="App">
-      <h1 className="Header">Characters</h1>
+     <h1>Starwars Characters</h1>
+      {character.map(element=> {
+        return(
+          <Character
+            name ={element.name}
+            gender ={element.gender}
+            height = {element.height}
+            haircolor ={element.hair_color}
+            skincolor ={element.skin_color}
+            mass ={element.mass}
+          />
+        )
+      })}
+      <Card>
+       <CardFooter>Copy Right Furno West</CardFooter>
+       </Card>
     </div>
+    
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,35 @@
 // Write your Character component here
+import React from "react";
+import { Card,  CardHeader, CardBody, CardText } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Character = ({name, gender,height,haircolor,skincolor,mass}) => {
+//    return(
+//        <div className="charList">
+//            <h2>Character Name: {name}</h2>
+//            <h3>Gender: {gender}</h3>
+//            <h3>Height: {height} </h3>
+//            <h3>Hair Color: {haircolor}</h3>
+//            <h3>Skin Color: {skincolor}</h3>
+//            <h3>Mass: {mass}</h3>
+//        </div>
+//    ) 
+   return (
+    <div className="cardContainer">
+      <Card>
+      <CardHeader> <h2>Name: {name}</h2></CardHeader>
+        <CardBody>
+          <h4> Gender: {gender}</h4>
+          <h4>Height: {height}</h4>
+          <h4>Hair Color: {haircolor}</h4>
+          <h4>Skin Color: {skincolor}</h4>
+          <h4>Mass: {mass}</h4>
+          </CardBody>
+      </Card>
+      
+    </div>
+  );
+}
+
+
+export  default Character;
